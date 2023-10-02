3ee194da763308e1d5477f255b1b3d79bc462a54
Sudhanshu <tiwarisudhanshu363@gmail.com>
2023-10-02 23:06:53 +0530

    login edit

--- flaskblog.py
+++ flaskblog.py
@@ -48,7 +48,7 @@ def login():
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
