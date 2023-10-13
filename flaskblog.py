9d4165dc6550a7576270715e439d27c570cc9d30
Sudhanshu <tiwarisudhanshu363@gmail.com>
2023-10-13 23:54:36 +0530

    databse failed again

--- flaskblog.py
+++ flaskblog.py
@@ -81,4 +81,7 @@ def login():
 
 
 if __name__ == '__main__':
+    with app.app_context():  # Create an application context
+        db.create_all()  # Create database tables
+
     app.run(debug=True)
